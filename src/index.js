6de3a037a0ad690aa8bf8842e7a308b51561c090
Supesh Sinha <sinha.supesh@gmail.com>
2023-09-10 11:39:54 +0530

    footer added

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navb from './pages/components/navbar/Navb';
+import Footer from './pages/components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Navb/>
     <Routes>
-    <Route path="/" element={<Home />} />
-  </Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    <Footer/>
   </BrowserRouter>
 );
 
