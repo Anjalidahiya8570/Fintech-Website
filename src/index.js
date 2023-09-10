44d7ac86592dce73d59790e192fd41a5c3a742d7
Supesh Sinha <sinha.supesh@gmail.com>
2023-09-10 10:40:58 +0530

    navbar and carousal added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navb from './pages/components/navbar/Navb';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Navb/>
+    <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Carouselhome from './components/Carouselhome';
+
+const Home = () => {
+  return (
+    <Carouselhome/>
+  )
+}
+
+export default Home;
