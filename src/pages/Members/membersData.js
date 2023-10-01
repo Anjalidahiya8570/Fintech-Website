a1ef1567aa31272d1e868e46cbfd13ec823abe14
Supesh Sinha <sinha.supesh@gmail.com>
2023-10-02 00:49:08 +0530

    member workflow created

--- src/pages/Members/membersData.js
+++ src/pages/Members/membersData.js
@@ -1,31 +1,182 @@
 export default {
     fourthYear: [
         {
-            name: "Web developer",
-            email: "Creates website",
-            linkedin: "",
-            imgName: "",
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
         },
         {
-            name: "Web developer",
-            email: "Creates website",
-            linkedin: "",
-            imgName: "",
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
         }
+        
     ],
 
     thirdYear: [
         {
-            name: "Web developer",
-            email: "Creates website",
-            linkedin: "",
-            imgName: "",
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
         },
         {
-            name: "Web developer",
-            email: "Creates website",
-            linkedin: "",
-            imgName: "",
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        }
+    ],
+
+    secondYear: [
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        }
+    ],
+
+    alumni: [
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
+        },
+        {
+            name: "Harshit Jaiswal",
+            email: "harshitj.iitism@gmail.com",
+            linkedin: "https://www.linkedin.com/in/harshit-jaiswal-158a13190/",
+            imgName: "harshit.jpeg",
         }
     ]
 };
---
+++ src/pages/components/MemberContainer/MemberContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MemberContainer = (props) => {
+  return (
+    <div className="card-container">
+      
+    </div>
+  )
+}
+
+export default MemberContainer
